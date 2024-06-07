e77187e2916adb7794e4b80d3ffadda630f82733
Maxim Gritsenko <maxgritsenko2@gmail.com>
2024-06-07 13:19:01 -0400

    mostly finished clients section

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
+import dynamic from "next/dynamic";
 import Clients from "@/components/Clients";
-import Grid from "@/components/Grid";
+// import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import RecentProjects from "@/components/RecentProjects";
 import { FloatingNav } from "@/components/ui/FloatingNav";
 import { navItems } from "@/data";
-import { FaHome } from "react-icons/fa";
 
 export default function Home() {
+  const Grid = dynamic(() => import("@/components/Grid"), {
+    ssr: false,
+  });
+  
   return (
     <main className="relative bg-black-100 flex justify-center 
     items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
